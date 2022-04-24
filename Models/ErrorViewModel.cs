9ce5302dcb52d707504d0f1ad0ce5d8f9f6525d0
Jamil Usman <jamilusman32@gmail.com>
2022-04-24 09:07:03 +0700

    add dotnet core app sample

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace dotnetpipeline.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
